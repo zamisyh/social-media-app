a4c934112b630bf33885a75585c3c9ee92622f00
zam-dev <zamsyh.work@gmail.com>
2022-08-06 13:59:08 +0700

    Protect route login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
-import "./App.scss"
-import "./pages/home/Home.scss"
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
-import Auth from "./pages/auth/Auth";
+import './App.scss'
+import './pages/home/Home.scss'
+import Home from './pages/home/Home'
+import Profile from './pages/profile/Profile'
+import Auth from './pages/auth/Auth'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 function App() {
+  const user = useSelector((state) => state.AUTH_REDUCER.auth)
+
   return (
     <div className="App">
-       <div className="blur" style={{ top: '-18%', right: '0' }}></div>
-       <duv className="blur" style={{ top: '36%', left: '-8rem'  }}></duv>
-       {/* <Home /> */}
-       {/* <Profile /> */}
-       <Auth />
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <duv className="blur" style={{ top: '36%', left: '-8rem' }}></duv>
+
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route 
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth/>}
+        />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
